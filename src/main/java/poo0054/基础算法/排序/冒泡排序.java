c3b9877d58f452e0d0c6c0be3c54219506d2a520
zhangzhi <poo0054@outlook.com>
2022-08-15 10:06:18 +0800

    冒泡排序 从大到小

--- src/main/java/poo0054/基础算法/排序/冒泡排序.java
+++ src/main/java/poo0054/基础算法/排序/冒泡排序.java
@@ -13,13 +13,13 @@ public class 冒泡排序 {
     @Test
     public void Test() {
         int[] ints = {10, 9, 8, 7, 6, 5, 11, 12};
-        ints = bubbleSort(ints);
+        bubbleSortDesc(ints);
         Arrays.stream(ints).forEach(System.out::println);
     }
 
     // ==============================从小到大
 
-    private int[] bubbleSort(int[] ints) {
+    private void bubbleSort(int[] ints) {
         for (int i = 0; i < ints.length - 1; i++) {
             boolean temple = true;
             for (int j = i + 1; j < ints.length; j++) {
@@ -33,9 +33,11 @@ public class 冒泡排序 {
                 break;
             }
         }
-        return ints;
     }
 
+    /**
+     * 替换
+     */
     public static void exchange(int[] ints, int i, int j) {
         int temple = ints[j];
         ints[j] = ints[i];
@@ -45,9 +47,19 @@ public class 冒泡排序 {
     // ==============================从大到小
 
 
-    private int[] bubbleSortDesc(int[] ints) {
-
-        return new int[1];
+    public void bubbleSortDesc(int[] ints) {
+        for (int i = 0; i < ints.length - 1; i++) {
+            boolean b = true;
+            for (int j = i; j < ints.length; j++) {
+                if (ints[i] < ints[j]) {
+                    exchange(ints, i, j);
+                    b = false;
+                }
+            }
+            if (b) {
+                System.out.println("后面的都是有序的了  我直接跳过========" + (++i) + "  值为：" + ints[i]);
+                return;
+            }
+        }
     }
-
 }
